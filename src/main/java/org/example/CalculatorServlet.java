f2db123897a005c39b4240af35637b6a6a69f2f7
S-Onix <msyoo101@hansung.ac.kr>
2023-01-19 16:29:24 +0900

    Servlet to GenericServlet convert

--- src/main/java/org/example/CalculatorServlet.java
+++ src/main/java/org/example/CalculatorServlet.java
@@ -15,16 +15,9 @@ import java.io.PrintWriter;
  * Servlet(interface) -> GenericServlet(extends / service() 부분을 제외한 나머지는 기초적으로 구현되어 있음) -> HttpServlet 구조로 이루어짐
  * */
 @WebServlet("/calculate")
-public class CalculatorServlet implements Servlet {
+public class CalculatorServlet extends  GenericServlet {
 
     private static final Logger logger = LoggerFactory.getLogger(CalculatorServlet.class);
-    private ServletConfig servletConfig;
-    @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
-        // resource init
-        logger.info("init");
-        this.servletConfig = servletConfig;
-    }
 
     // /calculate?operand1=11&operator=*&operand2=22
     // [?] 이하 쿼리스트링 값을 request에서 getParameter로 가져올 수 잇음
@@ -48,22 +41,5 @@ public class CalculatorServlet implements Servlet {
         response.setCharacterEncoding("UTF-8");
         PrintWriter writer = response.getWriter();
         writer.println(result);
-
-
     }
-    @Override
-    public void destroy() {
-        // resource release
-        logger.info("destroy");
-    }
-    @Override
-    public ServletConfig getServletConfig() {
-        return this.servletConfig;
-    }
-    @Override
-    public String getServletInfo() {
-        return null;
-    }
-
-
 }
